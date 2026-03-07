d1aadf72e40e36e92ec0dbda452ea1b4c7d9a961
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-07 14:10:22 -0500

    Create changeUserPass.sh

---
+++ changeUserPass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EXCLUDE="root|nobody"
+
+for user in $(awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/passwd | grep -Ev "$EXCLUDE")
+do
+    newpass=$(openssl rand -base64 12)
+    echo "$user:$newpass" | chpasswd
+done
