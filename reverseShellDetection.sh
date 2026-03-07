e1de559c9d97e53e43d2cc68b4eddbb484f54569
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-07 14:47:47 -0500

    Create reverseShellDetection.sh

---
+++ reverseShellDetection.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOGFILE="/var/log/reverse_shell_detection.log"
+
+echo "==== Reverse Shell Scan $(date) ====" >> $LOGFILE
+
+echo "--- Suspicious Processes ---" >> $LOGFILE
+ps aux | grep -E "nc|netcat|ncat|socat|bash -i|python -c|perl -e|php -r" | grep -v grep >> $LOGFILE
+
+echo "--- Suspicious Network Connections ---" >> $LOGFILE
+ss -tunap | grep ESTAB >> $LOGFILE
+
+echo "--- Connections to External IPs ---" >> $LOGFILE
+ss -tunap | grep -E "ESTAB" | grep -v "127.0.0.1" >> $LOGFILE
+
+echo "--- Active Listeners ---" >> $LOGFILE
+ss -tulnp >> $LOGFILE
+
+echo "" >> $LOGFILE
+
+echo "Scan complete. Results logged to $LOGFILE"
