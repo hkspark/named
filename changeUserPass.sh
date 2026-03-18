17818cfd6ba4b306a0ea8ec86829a61f98867ef9
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-18 14:38:14 -0400

    Update changeUserPass.sh

--- changeUserPass.sh
+++ changeUserPass.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#Will output password to console, can change if need be
 
-EXCLUDE="cyberrange"
+EXCLUDE="cyberrange | greyteam"
 
 for user in $(awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/passwd | grep -Ev "$EXCLUDE")
 do
