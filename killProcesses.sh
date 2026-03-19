fd9e36f780088e7eaf2b139bd6b7e3509260e917
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-18 20:31:23 -0400

    Update killProcesses.sh

--- killProcesses.sh
+++ killProcesses.sh
@@ -3,7 +3,55 @@
 echo "Scanning processes..."
 
 # Allowed processes
-allowed=("vsftpd" "ftp" "ftpd" "systemd-journal" "systemd-udevd" "systemd-resolve" "systemd-timesyn" "dbus-daemon" "polkitd" "systemd-logind" "rsyslogd" "sd-pam" "scsi_eh_0" "scsi_eh_1" "bash" "sshd" "ssh" "dbus" "NetworkManager" "polkit" "qemu-guest-agent"  "polkitd")
+allowed=("apparmor.service" | 
+"cloud-config.service" | 
+"cloud-final.service" | 
+"keyboard-setup.service" | 
+"networkd-dispatcher.service" | 
+"kmod-static-nodes.service" | 
+"fwupd.service" | 
+"cloud-init-local.service" | 
+"cloud-init.service" | 
+"console-setup.service" | 
+"vsftpd.service" | 
+"systemd-journald.service" | 
+"systemd-udevd" |
+"systemd-resolved.service" | 
+"systemd-timesyn" | 
+"systemd-binfmt.service" | 
+"systemd-journal-flush.service" | 
+"systemd-logind.service" | 
+"systemd-modules-load.service" | 
+"systemd-remount-fs.service" | 
+"systemd-sysctl.service" | 
+"systemd-tmpfiles-setup-dev-early.service" |
+"systemd-tmpfiles-setup-dev.service" |
+"systemd-tmpfiles-setup.service" |
+"systemd-udev-settle.service" |
+"systemd-udev-trigger.service" |
+"systemd-udevd.service" |
+"systemd-update-utmp.service" |
+"systemd-user-sessions.service" |
+"udisks2.service" |
+"ufw.service" |
+"upower.service" |
+"wazuh-agent.service" |
+"wpa_supplicant.service" |
+"dbus-daemon"  |
+"systemd-logind" |
+"rsyslogd" |
+"sd-pam" |
+"scsi_eh_0" | 
+"scsi_eh_1" |
+"bash" |
+"sshd" |
+"ssh" |
+"dbus.service" |
+"NetworkManager" |
+"polkit.service" |
+"qemu-guest-agent.service"  
+"polkitd.service" | 
+"rsyslog.service" | )
 
 for pid in $(ps -eo pid=); do
     proc=$(ps -p $pid -o comm= 2>/dev/null)
