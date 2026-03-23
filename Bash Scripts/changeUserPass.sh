319ea4e6b77b26961bc7d7a6e14731a0033951f2
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-23 13:41:04 -0400

    Update changeUserPass.sh

--- Bash Scripts/changeUserPass.sh
+++ Bash Scripts/changeUserPass.sh
@@ -2,7 +2,7 @@
 #Will output password to console, can change if need be
 #Changes all users passwords except cyberrange and greyteam to random string
 
-EXCLUDE="GREYTEAM | scoring | ansible"
+EXCLUDE="GREYTEAM | scoring | ansible | deliveryDriver"
 
 for user in $(awk -F: '$3 >= 1000 && $3 < 65534 {print $1}' /etc/passwd | grep -Ev "$EXCLUDE")
 do
