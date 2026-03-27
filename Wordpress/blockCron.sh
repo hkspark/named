88b377bc678456cada3e5dc050b8b8ca33446765
hkspark <77894884+hkspark@users.noreply.github.com>
2026-03-27 10:53:30 -0400

    Create blockCron.sh

---
+++ Wordpress/blockCron.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Stops all cron jobs that are running and changes permissions to cron files so they won't run again
+
+echo "Removing cron jobs..."
+
+# Remove user cron jobs
+rm -rf /var/spool/cron/*
+rm -rf /var/spool/cron/crontabs/*
+
+# Remove system cron jobs
+rm -f /etc/cron.d/*
+rm -f /etc/cron.daily/*
+rm -f /etc/cron.hourly/*
+rm -f /etc/cron.weekly/*
+rm -f /etc/cron.monthly/*
+
+echo "Locking cron directories..."
+
+chmod 000 /var/spool/cron
+chmod 000 /var/spool/cron/crontabs
+chmod 000 /etc/cron.d
+
+echo "Cron persistence blocked."
